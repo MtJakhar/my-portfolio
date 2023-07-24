d488d8d7fd2f3299a8835325285103e802d70968
SquatingCyrus <thvsd.10fingers@gmail.com>
2023-07-24 16:09:01 -0400

    fixed logo

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -4,14 +4,17 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import Image from "next/image";
+import logo from "../../public/images/logo.png";
 
 const NavBar = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="sticky" color="transparent">
         <Toolbar>
+          <Image src={logo} width={50} height={50} />
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            ICON
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
