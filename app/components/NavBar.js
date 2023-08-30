1c63f073a5d27d0ff33e178f572bbd19478a287a
SquatingCyrus <thvsd.10fingers@gmail.com>
2023-08-30 11:20:12 -0400

    removed button

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -2,7 +2,6 @@ import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import Image from "next/image";
 import logo from "../../public/images/logo.png";
 import NavButtons from "./NavButtons";
