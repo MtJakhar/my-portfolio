1860defdc2d1de0caf58ff0bacaeea58a679a6ad
SquatingCyrus <thvsd.10fingers@gmail.com>
2023-09-05 12:37:00 -0400

    added useState to About to make image dissapear

--- app/components/About.js
+++ app/components/About.js
@@ -1,25 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import avatar from "../../public/images/avatar.png";
-import Grid from "@mui/material/Grid";
 
 const About = () => {
+  const [isImageVisible, setIsImageVisible] = useState(true);
+
+  const toggleVisiblity = () => {
+    if (window.innerWidth < 768) {
+      setIsImageVisible(false);
+    } else {
+      setIsImageVisible(true);
+    }
+  };
+
+  useEffect(() => {
+    toggleVisiblity();
+    window.addEventListener("resize", toggleVisiblity);
+    return () => {
+      window.removeEventListener("resize", toggleVisiblity);
+    };
+  }, []);
+
   return (
-    <div className="lg:pb-[400px] md:pb-[250px] sm:pb-[250px]">
-      <Grid container rowSpacing={2} columnSpacing={1}>
-        <Grid item xs={12} md={6}>
-          <div className="text-5xl text-indigo-900  leading-loose ml-28">
-            <p>
-              My Name is <span className="text-fuchsia-700">Muizz</span> Jakhar
-              I am a <span className="text-fuchsia-700">Full-Stack </span> Web
-              Developer
-            </p>
-          </div>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Image src={avatar} alt="Muizz Jakhar Avatar" />
-        </Grid>
-      </Grid>
+    <div className="lg:pb-[400px] md:pb-[250px] sm:pb-[250px] grid gap-4 md:grid-cols-2 ">
+      <p className="text-5xl text-indigo-900  leading-loose ml-28">
+        My Name is <span className="text-fuchsia-700">Muizz</span> Jakhar I am a{" "}
+        <span className="text-fuchsia-700">Full-Stack </span> Web Developer
+      </p>
+      {isImageVisible && <Image src={avatar} alt="Muizz Jakhar Avatar" />}
     </div>
   );
 };
