9cae60bb18e114a72e62fe87bff2d9f1bf11d48f
SquatingCyrus <thvsd.10fingers@gmail.com>
2023-09-07 13:19:58 -0400

    fixed spacing in about

--- app/components/About.js
+++ app/components/About.js
@@ -24,8 +24,8 @@ const About = () => {
   return (
     <div className="sm:pb-[300px] md:pb-[250px] lg:pb-[400px] grid gap-4 md:grid-cols-2 mx-auto">
       <p className=" text-indigo-900 sm:text-center text-left text-5xl leading-relaxed mx-auto my-auto px-10">
-        My Name is <span className="text-fuchsia-700">Muizz</span> Jakhar I am a{" "}
-        <span className="text-fuchsia-700  ">Full-Stack </span> Web Developer
+        My Name is <span className="text-fuchsia-700">Muizz</span> Jakhar I am a
+        <span className="text-fuchsia-700  "> Full-Stack </span> Web Developer
       </p>
       {isImageVisible && (
         <Image
